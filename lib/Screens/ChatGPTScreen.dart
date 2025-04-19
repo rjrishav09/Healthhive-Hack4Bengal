0836a58e88578dff084713db36253f3c4b8728ad
rjrishav09 <crazyrishavhacker@gmail.com>
2025-04-19 10:35:24 +0530

    Initial commit

--- lib/Screens/ChatGPTScreen.dart
+++ lib/Screens/ChatGPTScreen.dart
@@ -29,7 +29,7 @@ class _ChatGptScreenState extends State<ChatGptScreen> {
   @override
   void initState() {
     super.initState();
-    OpenAI.instance.build(token:"",baseOption:
+    OpenAI.instance.build(token:" ",baseOption:
     HttpSetup(receiveTimeout: const Duration(seconds: 5)),isLogger: true);
   }
 
